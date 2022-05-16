e602945a8df5c00b1a34948f403a88a7e3efb048
Haley Telega <haleytelega@outlook.com>
2022-05-16 09:49:16 -0400

    added product model

--- models/Product.js
+++ models/Product.js
@@ -9,14 +9,37 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+  id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+  },
+  product_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+      validate: {
+        len: [1]
+      }
+  },
+  price: {
+    type: DataTypes.DECIMAL,
+    allowNull: false
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  category_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: 'product', 
   }
 );
 
